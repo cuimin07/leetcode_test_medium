543bc3be600ad1c97731e56bb6ddec41cca308b2
cuimin07 <48578114+cuimin07@users.noreply.github.com>
2020-02-05 18:02:33 +0800

    算法题-两两交换链表中的节点

---
+++ 014.两两交换链表中的节点.py
@@ -0,0 +1,24 @@
+'''
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+示例:
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+'''
+
+#答：【递归】
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        first_node = head
+        second_node = head.next
+        first_node.next = self.swapPairs(second_node.next)
+        second_node.next = first_node
+        return second_node
